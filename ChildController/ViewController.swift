1057b59c1fae2a11fe82739db1d658314555e5ec
Henrique Valcanaia <henriqueindalencio+github@gmail.com>
2020-04-17 18:01:26 -0300

    Initial Commit

---
+++ ChildController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChildController
+//
+//  Created by Henrique Valcanaia on 2020-04-17.
+//  Copyright © 2020 Henrique Valcanaia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
